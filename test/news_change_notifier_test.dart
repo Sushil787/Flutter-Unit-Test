9fd311e26dfcbd2191c24cc812cf1d3911f22336
Sushil787 <itsushil100@outlook.com>
2022-12-28 12:34:48 +0545

    added: test case for articles

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -1,62 +1,64 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_tutorial/article.dart';
 import 'package:flutter_testing_tutorial/news_change_notifier.dart';
 import 'package:flutter_testing_tutorial/news_service.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockNewsService extends Mock implements NewsService {}
+// class BadMockNewsServices implements NewsService {
+//   bool getArticleCalled = false;
+//   @override
+//   Future<List<Article>> getArticles() async {
+//     getArticleCalled = true;
+
+//     return [
+//        Article(title: "Test 1", content: "Test 1 content"),
+//       Article(title: "Test 2", content: "Test 2 content"),
+//       Article(title: "Test 3", content: "Test 3 content"),
+//     ];
+//   }
+// }
+
+class MockNewsServices extends Mock implements NewsService {}
 
 void main() {
   late NewsChangeNotifier sut;
-  late MockNewsService mockNewsService;
+  late MockNewsServices mockNewsService;
 
-  setUp(() {
-    mockNewsService = MockNewsService();
+  setUp((() {
+    mockNewsService = MockNewsServices();
     sut = NewsChangeNotifier(mockNewsService);
+  }));
+  test("initial value are correct", () {
+    expect(sut.articles, []);
+    expect(sut.isLoading, false);
   });
-
-  test(
-    "initial values are correct",
-    () {
-      expect(sut.articles, []);
-      expect(sut.isLoading, false);
-    },
-  );
-
-  group('getArticles', () {
-    final articlesFromService = [
-      Article(title: 'Test 1', content: 'Test 1 content'),
-      Article(title: 'Test 2', content: 'Test 2 content'),
-      Article(title: 'Test 3', content: 'Test 3 content'),
+  group("get articles", (() {
+    final getArticleServices = [
+      Article(title: "Test 1", content: "Test 1 content"),
+      Article(title: "Test 2", content: "Test 2 content"),
+      Article(title: "Test 3", content: "Test 3 content"),
     ];
-
-    void arrangeNewsServiceReturns3Articles() {
-      when(() => mockNewsService.getArticles()).thenAnswer(
-        (_) async => articlesFromService,
-      );
+    void arrangeNewsServicesReturns3Articles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((_) async => getArticleServices);
     }
 
+    test("get article using news services", () async {
+      arrangeNewsServicesReturns3Articles();
+      await sut.getArticles();
+      verify(() => mockNewsService.getArticles()).called(1);
+    });
     test(
-      "gets articles using the NewsService",
-      () async {
-        arrangeNewsServiceReturns3Articles();
-        await sut.getArticles();
-        verify(() => mockNewsService.getArticles()).called(1);
-      },
-    );
-
-    test(
-      """indicates loading of data,
-      sets articles to the ones from the service,
-      indicates that data is not being loaded anymore""",
-      () async {
-        arrangeNewsServiceReturns3Articles();
-        final future = sut.getArticles();
-        expect(sut.isLoading, true);
-        await future;
-        expect(sut.articles, articlesFromService);
-        expect(sut.isLoading, false);
-      },
-    );
-  });
+        '''indicates loading of data, set article o one from the service, indicates the data is not be load''',
+        (() async {
+      arrangeNewsServicesReturns3Articles();
+      final future = sut.getArticles();
+      expect(sut.isLoading, true);
+      await future;
+      expect(sut.articles, getArticleServices);
+      expect(sut.isLoading, false);
+    }));
+  }));
 }
